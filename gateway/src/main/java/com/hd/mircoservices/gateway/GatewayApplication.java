a9c4048ad0f6c71fbb57945571b5ff709fa8cff0
Hu Le <lhu@thoughtworks.com>
2017-07-19 20:07:59 +0800

    [T] add spring session

--- gateway/src/main/java/com/hd/mircoservices/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/hd/mircoservices/gateway/GatewayApplication.java
@@ -1,14 +1,51 @@
 package com.hd.mircoservices.gateway;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.savedrequest.NullRequestCache;
+import org.springframework.session.web.http.HeaderHttpSessionStrategy;
+import org.springframework.session.web.http.HttpSessionStrategy;
 
 @SpringBootApplication
 @EnableZuulProxy
 public class GatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @EnableGlobalMethodSecurity
+    public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Autowired
+        public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+            auth
+                    .inMemoryAuthentication()
+                    .withUser("admin").password("password").roles("USER");
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests()
+                    .anyRequest().authenticated()
+                    .and()
+                    .requestCache()
+                    .requestCache(new NullRequestCache())
+                    .and()
+                    .httpBasic();
+        }
+
+        @Bean
+        public HttpSessionStrategy httpSessionStrategy() {
+            return new HeaderHttpSessionStrategy();
+        }
+    }
 }
